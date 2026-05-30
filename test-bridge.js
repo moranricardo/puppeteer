a1b4bfbfa003158166ebb0f26e1735dde32340e8
Ricardo Moran <152412958+moranricardo@users.noreply.github.com>
2026-05-30 14:31:42 -0700

    actualizar test-bridge.js

--- test-bridge.js
+++ test-bridge.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+ puppeteer = require('puppeteer');
 
 async function checkBridge() {
   console.log("Iniciando validación de puente entre octocromo y titiritero...");
