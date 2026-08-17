48c482ec1bb3644a333c070643e1e1085f04f5c0
moranricardo <152412958+moranricardo@users.noreply.github.com>
2026-08-17 09:31:49 -0700

    Actualizar configuración de rollup, estado mejorado y archivos compilados

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,39 @@
 /**
  * @license
- * Copyright 2024 Google Inc.
+ * Copyright 2024-2026 Google Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'main.mjs',
-  output: {
-    format: 'esm',
-    dir: 'out',
-    sourcemap: true,
-  },
-  external: ['chromium-bidi/lib/cjs/bidiMapper/BidiMapper.js'],
+  output: [
+    {
+      file: 'out/bundle.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'out/bundle.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    }
+  ],
+  external: [
+    'chromium-bidi/lib/cjs/bidiMapper/BidiMapper.js',
+    /^node:/
+  ],
   plugins: [
     nodeResolve({
       browser: true,
+      preferBuiltins: false,
       resolveOnly: ['puppeteer-core'],
     }),
-    terser(),
+    commonjs(),
   ],
+  onwarn(warning, warn) {
+    if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+    warn(warning);
+  }
 };
